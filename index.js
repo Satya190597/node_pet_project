2c202664318935c0854e6befa7ee8085e92b38b0
satya19051997 <satya.nandy@evossolutions.com>
2019-04-30 00:41:49 +0530

    Added eslint for maintaining good coding standards

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const port = 8088;
+const port = 8088
 
-app.get('/',(request,response) => {
-    response.status(200).json({"message":"Welcome To My Application"});
-});
+app.get('/', (request, response) => {
+  response.status(200).json({ 'message': 'Welcome To My Application' })
+})
 
-app.listen(port,(request,response) => {
-    console.log("Server Is Running");
+app.listen(port, (request, response) => {
+  console.log('Server Is Running')
 })
