9f795fcf97d42e930dcb02ec3889ed83d3fdfb3d
satya19051997 <satya.nandy@evossolutions.com>
2019-05-17 20:56:27 +0530

    Added Config Example

---
+++ config-example.js
@@ -0,0 +1,8 @@
+const config = {
+    port: '<port-number>',
+    connectionUrl: 'mongodb+srv://<username>:<password>@cluster0-zokmm.mongodb.net/test?retryWrites=true',
+    connectionOption: { useNewUrlParser: true }
+  }
+  
+  module.exports = config
+  
