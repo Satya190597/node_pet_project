b17dd13274ed8a447ea1dd12fe2d2c7b68869959
satya19051997 <satya.nandy@evossolutions.com>
2019-05-17 16:48:20 +0530

    Added User Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,11 +6,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/Satya', { useNewUrlParser: true })
 var newUser = new User({
     firstName : 'Satya Prakash',
     lastName : 'Nandy',
-    description : 'Best Web Developer'
+    email : 'nandy@yahoo.in',
+    password : 'test'
 })
 
-newUser.save(function(err){
-    if(err)
-     return 'went';
-     mongoose.connection.close();
+newUser.save().then((user) => {
+    console.log(user)
+}).catch((error) => {
+    console.log({'error':error})
 })
