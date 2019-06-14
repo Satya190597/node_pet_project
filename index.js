09aeb18e028ab043081a0e4823779d934d186deb
satya190597 <webprakashsatya@gmail.com>
2019-06-14 16:32:38 +0530

    Random Commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+// const bodyParser = require('body-parser')
+const routes = require('./routes/route')
 
 const app = express()
 
 const port = 8088
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+app.use(routes)
 
 app.get('/', (request, response) => {
   response.status(200).json({ 'message': 'Welcome To My Application' })
